6a5d7a6669921841372b0ff8a4e3a714036c9102
Rafael Santos <rafa.05125@gmail.com>
2023-06-27 11:18:49 -0300

    Domain exception

---
+++ Accounts/exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+namespace Accounts.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
